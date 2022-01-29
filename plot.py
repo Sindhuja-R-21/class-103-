d07299e8b8eaa1573026948f06f04f46d2ecdf1c
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-01-29 07:19:39 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,9 @@
+from turtle import color, title
+import pandas as pd
+import plotly.express as px
+
+df=pd.read_csv("line_chart.csv")
+
+fig=px.line(df,x="Year",y="Per capita income",color="Country",title="Per Captia Income")
+
+fig.show()
